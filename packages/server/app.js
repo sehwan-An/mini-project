9cb6cfdc56fa50c63793bdf639aadae6c41c0d23
302-09\HANUL <sehwan4104@gmail.com>
2025-03-31 11:02:09 +0900

    express 미들웨어 추가(=장착)
    1.json 미들웨어
    2.mulitipart/formdata 미들웨어

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
+
 const app = express()
 import connect from './schemas/index.js'
-
 import indexRouter from './routes/index.js'
 import userRouter from './routes/user.route.js'
-connect(process.env.DB_URI)
 
+connect()
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/',indexRouter)
 app.use('/users', userRouter); 
 // connect('mongodb://admin:0000@localhost:27017')
