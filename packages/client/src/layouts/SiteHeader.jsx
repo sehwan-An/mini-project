398c95092e8b312673a0ca3ffa43811117e71cc2
302-09\HANUL <sehwan4104@gmail.com>
2025-04-01 13:06:20 +0900

    라우터 컴포넌트 레이아웃
    1.해더 레이아웃
    
    2.푸터 레이아웃

---
+++ packages/client/src/layouts/SiteHeader.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import {Container, Row, Col, Button} from 'react-bootstrap'
+import siteLogo from '../assets/site-logo.png'
+import { NavLink } from 'react-router'
+
+
+const SiteHeader = () => {
+  const handleClick = () => {
+   alert('회원가입으로 이동합니다') 
+  }
+    return (
+    <header className='py-3'>
+        <Container> 
+<Row className='align-items-center justify-content-between'>
+    <Col className='logo'>
+<img src={siteLogo} alt="logo" />
+    </Col>
+    <Col className='d-flex align-items-center gap-3 '>
+    <nav>
+        <ul className='d-flex gap-3'>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/'>about</NavLink></li>
+            <li><NavLink to='/'>community</NavLink></li>
+        </ul>
+
+    </nav>
+    <Button onClick={handleClick} >Register Now <i class="bi bi-arrow-right"></i></Button>
+    </Col>
+</Row>
+        </Container>
+    </header>
+  )
+}
+
+export default SiteHeader
