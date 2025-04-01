355f479afab8095b1c5a952aba8e71d4b568b023
302-09\HANUL <sehwan4104@gmail.com>
2025-04-01 15:28:22 +0900

    Home caruosel 작업

---
+++ packages/client/src/components/HomeCarousel.jsx
@@ -0,0 +1,36 @@
+import {Container, Carousel} from 'react-bootstrap/Carousel';
+import HomeImage from '../assets/home-section.png'
+
+function HomeCarousel() {
+  return (
+    <Container>
+      <Carousel>
+        <Carousel.Item>
+          <img src={HomeImage} alt='home'className='img-fluid'/>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={HomeImage} alt='home'/>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={HomeImage} alt='home'/>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  );
+}
+
+export default HomeCarousel;
