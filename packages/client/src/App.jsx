294c215d3020484b8949779bb5615e5e6ac4f818
302-09\HANUL <sehwan4104@gmail.com>
2025-04-01 13:07:49 +0900

    의존성 패키지 추가

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import {Routes, Route} from 'react-router'
 import Home from './routes/Home'
+import About from './routes/About.jsx'
 import SignUp from './routes/SignUp'
+import SignIn from './routes/SignIn'
+import Board from './routes/Board'
 
 const App = () => {
   return (
     < >
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Home />}>
+      <Route path='/signup' element={<About />}/>
       <Route path='/signup' element={<SignUp />}/>
+      <Route path='/signup' element={<SignIn />}/>
+      <Route path='/signup' element={<Board />}/>
+      </Route>
       </Routes>
     </>
   )
