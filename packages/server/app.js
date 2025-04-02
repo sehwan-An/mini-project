60fcd53066c70da9e267f1243aca85ad8140dc33
302-09\HANUL <sehwan4104@gmail.com>
2025-04-02 17:47:49 +0900

    1.morgan
    2.cors
    추가

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,11 +1,15 @@
 import express from 'express'
 
 const app = express()
+import logger from 'morgan'
+import cors from 'cors'
 import connect from './schemas/index.js'
 import indexRouter from './routes/index.js'
 import userRouter from './routes/user.route.js'
 
 connect()
+app.use(logger('dev'))
+app.use (cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/',indexRouter)
