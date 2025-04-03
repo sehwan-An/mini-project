01656b683b34bec1f750dceb57a0b624d36c43b1
302-09\HANUL <sehwan4104@gmail.com>
2025-04-03 13:04:33 +0900

    Hash 추가

--- packages/server/models/User.js
+++ packages/server/models/User.js
@@ -1,26 +1,34 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = mongoose.Schema({
-    username: {
-        type:String,
-        rquierd:true
-    },
-    pwd: {
-        type:String,
-        requierd: true,
-        length:12
-
-    },
-    email: {
-        type:String,
-        requierd:true,
-        unique: true
-    },
-    phone:{
-        type:String,
-    },
-    sns: String
-})
+  username: {
+    type: String,
+    rquierd: true,
+  },
+  pwd: {
+    type: String,
+    requierd: true,
+    length: 12,
+  },
+  email: {
+    type: String,
+    requierd: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+  },
+  sns: String,
+});
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.pwd = await bcrypt.hash(this.pwd, salt);
+  } catch (err) {
+    next(err);
+  }
+});
 
 const User = mongoose.model('User', userSchema);
 
