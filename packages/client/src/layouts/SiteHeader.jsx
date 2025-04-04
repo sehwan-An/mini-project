cbbc2e81f8d492ed40d1cdf7a681c067f6321cb3
302-09\HANUL <sehwan4104@gmail.com>
2025-04-04 13:07:04 +0900

    로그인 유지 처리
    1.네비게이션 변경: 로그인/로그아웃 메인메뉴 UI분리
    
    2. 쿠키 유무에 따라 로그인/로그아웃 처리
    
    3.쿠키삭제 로그아웃

--- packages/client/src/layouts/SiteHeader.jsx
+++ packages/client/src/layouts/SiteHeader.jsx
@@ -6,7 +6,7 @@ import siteLogo from '../assets/site-logo.png'
 import { NavLink, Navigate } from 'react-router'
 
 
-const SiteHeader = () => {
+const SiteHeader = ({userInfo, handleLogOut}) => {
   const handleClick = () => {
     location.href='/signup'
     
@@ -14,6 +14,22 @@ const SiteHeader = () => {
   }
     return (
     <header className='py-3'>
+      <Container>
+        <div className='w-100 text-end border-bottom py-2'>{ userInfo ?
+          (<>
+          <span>{userInfo}님 로그인중</span> {" | "}
+          <NavLink to='/modify'>정보수정</NavLink>{" "}
+          <NavLink onClick={handleLogOut}>로그아웃</NavLink>
+          
+          </>
+        ) : (
+          <>
+          <NavLink to='/signin'>로그인</NavLink> {" "}
+          <Button onClick={handleClick}>Register Now<i className=""/></Button>
+          </>
+        )}
+      </div>
+      </Container>
         <Container> 
 <Row className='align-items-center justify-content-between'>
     <Col className='logo'>
@@ -24,12 +40,10 @@ const SiteHeader = () => {
         <ul className='d-flex gap-3'>
             <li><NavLink to='/'>Home</NavLink></li>
             <li><NavLink to='/'>about</NavLink></li>
-            <li><NavLink to='/'>community</NavLink></li>
-            <li><NavLink to='SignIn'>로그인</NavLink></li>
+            <li><NavLink to='/'>board</NavLink></li>
         </ul>
 
     </nav>
-    <Button onClick={handleClick}>Register Now <i className="bi bi-arrow-right"></i></Button>
     </Col>
 </Row>
         </Container>
