c0c050ab22793995860f980ecbc4f5ce30888816
302-09\HANUL <sehwan4104@gmail.com>
2025-04-04 13:10:30 +0900

    cookiesParser 추가

--- packages/server/app.js
+++ packages/server/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 const app = express()
 import logger from 'morgan'
+import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import connect from './schemas/index.js'
 import indexRouter from './routes/index.js'
@@ -9,11 +10,16 @@ import userRouter from './routes/user.route.js'
 
 connect()
 app.use(logger('dev'))
-app.use (cors())
+app.use(cookieParser())
+app.use (cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/',indexRouter)
 app.use('/users', userRouter); 
+// app.use('/posts', postRouter)
 // connect('mongodb://admin:0000@localhost:27017')
 
 
