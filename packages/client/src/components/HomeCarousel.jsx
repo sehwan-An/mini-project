e005f45b38da3fe3b585e02b26fb46c05fa03e65
302-09\HANUL <sehwan4104@gmail.com>
2025-04-07 18:00:19 +0900

    className w-100추가

--- packages/client/src/components/HomeCarousel.jsx
+++ packages/client/src/components/HomeCarousel.jsx
@@ -3,24 +3,24 @@ import HomeImage from '../assets/home-section.png'
 
 function HomeCarousel() {
   return (
-    <Container>
+    <Container className='my-3 px-0' fluid={true}>
       <Carousel>
         <Carousel.Item>
-          <img src={HomeImage} alt='home'className='img-fluid'/>
+          <img src={HomeImage} alt='home'className='w-100 img-fluid'/>
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-        <img src={HomeImage} alt='home'className='img-fluid'/>
+        <img src={HomeImage} alt='home'className='w-100 img-fluid'/>
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-        <img src={HomeImage} alt='home'className='img-fluid'/>
+        <img src={HomeImage} alt='home'className='w-100 img-fluid'/>
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
