8165ec79edb9f0d46e72f7b4a406e1b47becfd43
302-09\HANUL <sehwan4104@gmail.com>
2025-04-07 12:01:02 +0900

    user 모델 스키마 변경
    1.timestamps 추가: mongoose에서 createdAt, updateAt 속성추가

--- packages/server/models/User.js
+++ packages/server/models/User.js
@@ -1,26 +1,29 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    rquierd: true,
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      rquierd: true,
+    },
+    password: {
+      type: String,
+      requierd: true,
+      length: 12,
+    },
+    email: {
+      type: String,
+      requierd: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    sns: String,
   },
-  password: {
-    type: String,
-    requierd: true,
-    length: 12,
-  },
-  email: {
-    type: String,
-    requierd: true,
-    unique: true,
-  },
-  phone: {
-    type: String,
-  },
-  sns: String,
-});
+  { timestamps: true }
+);
 userSchema.pre('save', async function (next) {
   try {
     const salt = await bcrypt.genSalt(10);
