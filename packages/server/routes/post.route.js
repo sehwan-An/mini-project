ef5b6dd0f6325bf0b0cb549a76e35009d5b61bc9
302-09\HANUL <sehwan4104@gmail.com>
2025-04-08 16:38:16 +0900

    추가사항 router.delete 추가

--- packages/server/routes/post.route.js
+++ packages/server/routes/post.route.js
@@ -4,8 +4,10 @@ import * as postController from '../controllers/post.controller.js'
 const router = express.Router()
 
 router.post('/posts', postController.createPosts)
-
 router.get('/posts', postController.readPosts) 
+router.get('/posts/:id', postController.readPost) 
+router.delete('/posts/:id', postController.removePost) 
+// router.put('/posts/:id', postController.updatePost)
 
 
 export default router
