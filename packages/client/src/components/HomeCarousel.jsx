0fd04d01e28e34a2e1f820bc785df068669167db
302-09\HANUL <sehwan4104@gmail.com>
2025-04-08 16:41:55 +0900

    홈캐러셀 이미지 수정

--- packages/client/src/components/HomeCarousel.jsx
+++ packages/client/src/components/HomeCarousel.jsx
@@ -1,26 +1,27 @@
 import {Container, Carousel} from 'react-bootstrap';
 import HomeImage from '../assets/home-section.png'
+import CarouselItemImage from './CarouselItemImage'
 
 function HomeCarousel() {
   return (
     <Container className='my-3 px-0' fluid={true}>
-      <Carousel>
+      <Carousel fade>
         <Carousel.Item>
-          <img src={HomeImage} alt='home'className='w-100 img-fluid'/>
+          <CarouselItemImage src="https://picsum.photos/id/1000/1920/400" text="picsum" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-        <img src={HomeImage} alt='home'className='w-100 img-fluid'/>
+        <CarouselItemImage src="https://picsum.photos/id/1002/1920/400" text="picsum" />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-        <img src={HomeImage} alt='home'className='w-100 img-fluid'/>
+        <CarouselItemImage src="https://picsum.photos/id/1001/1920/400" text="picsum" />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
