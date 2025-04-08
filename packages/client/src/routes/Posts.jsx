3e369657f06d92e4b46decf2a11c95c2358481ea
302-09\HANUL <sehwan4104@gmail.com>
2025-04-08 16:40:25 +0900

    글쓰기 추가 goPostWrite

--- packages/client/src/routes/Posts.jsx
+++ packages/client/src/routes/Posts.jsx
@@ -1,52 +1,84 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { Button } from 'react-bootstrap'
+import {Container,Row,Col} from 'react-bootstrap'
+import {useNavigate, Link} from 'react-router'
 
 const Posts = () => {
-  const [posts, setPosts] = useState(null);
-  const [loading, setLoading] = useState(true);
-  // if(loading){
-  //     return (
-  //         <div>로딩중....</div>
-  //     )
-  // }
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/api/posts", {
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log(res);
-        setLoading(false);
-        setPosts(res.data);
-      })
-      .catch((err) => console.log(err.message));
-  }, []);
-  return (
-    <div className="py-5">
-      {loading && <div>로딩중...</div>}
-      <table className="table table-striped w-75 mx-auto">
-        <thead>
-            <tr>
-                <th>번호</th>
-                <th>제목</th>
-                <th>글쓴이</th>
-                <th>작성일</th>
-            </tr>
-        </thead>
-        <tbody>
-      {posts &&
-        posts.map((post,i) => 
-            <tr key={i}>
-            <td>{i+1}</td> 
-            <td>{post.title}</td> 
-             <td>{post.author}</td> 
-             <td>{new Date().toLocaleDateString()}</td>
-          </tr>
-        )}
-        </tbody>
-        </table>
-    </div>
-  );
-};
+    const [posts, setPosts] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [isAuth, setIsAuth] = useState(false)
+    let navigate= useNavigate()
 
-export default Posts;
+    // if(loading){
+    //     return (
+    //         <div>로딩중....</div>
+    //     )
+    // }
+    useEffect(() => {
+      const token = Cookies.get("nexcent")
+      if(token){
+        setIsAuth(true)
+      }
+        axios
+            .get('http://localhost:3000/api/posts', {
+                withCredentials: true,
+            })
+            .then((res) => {
+                console.log(res)
+                setLoading(false)
+                setPosts(res.data)
+            })
+            .catch((err) => console.log(err.message))
+    }, [])
+    function convertDate(date){ 
+      return new Date(date).toLocaleDateString('ko-KR', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+    }).replaceAll(',','').replaceAll(' ','-')
+    }
+    function goPostWrite(){
+navigate('/addpost')
+    }
+    return (
+        <Container className="py-5">
+            {loading && <div>로딩중...</div>}
+            <Row>
+              <h2 className='text-center mb-3'>커뮤니티 | 글목록</h2>
+              <table className="table table-striped">
+                  <thead>
+                      <tr>
+                          <th>번호</th>
+                          <th>제목</th>
+                          <th>글쓴이</th>
+                          <th>작성일</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {posts &&
+                          posts.map((post, i) => (
+                              <tr key={i}>
+                                  <td>{i + 1}</td>
+                                  <td><Link to={`/post/${post.uuid}`}>{post.title}</Link></td>
+                                  <td>{post.author.username}</td>
+                                  <td>
+                                      {convertDate(post.createdAt)}
+              
+                                  </td>
+                              </tr>
+                          ))}
+                  </tbody>
+              </table>
+            </Row>
+            <Row className='justify-content-end'>
+              <Col md={2}>
+              {isAuth && <button onClick={goPostWrite}>글쓰기</button>}
+              </Col>
+              </Row>
+        </Container>
+    )
+}
+
+export default Posts
